28ed2a144ca7565bc56a61f5e3ab7b34b1bf48d0
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-11-09 21:11:57 -0500

    comments model startup

--- models/Comments.js
+++ models/Comments.js
@@ -2,3 +2,36 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Comment extends Model {}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+
+    comment: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        //add references to user
+    },
+
+    post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        //add reference to post
+    },
+
+    sequelize,
+    underscore: true,
+    modelName: 'comment',
+
+})
+
+module.exports = Comment;
